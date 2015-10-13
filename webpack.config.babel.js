c9b213b9005200f69d61a5e13f07c601918ac8d1
Adam Beck <AdamB.Infinity@gmail.com>
2015-10-13 01:48:31 -0400

    Add React to the project

---
+++ webpack.config.babel.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(ROOT_PATH, 'app');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  entry: APP_PATH,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      port: 3000
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
